051237343c46bda3f4f653d78af16f0e18f72f84
Leo <54992384+frankeu@users.noreply.github.com>
2020-10-31 09:02:02 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -28,7 +28,7 @@ function to_time_ago( $time ) {
         $div = $diff / $secs; 
         if( $div >= 1 ) {
             $t = round( $div );
-            return $t . ' ' . $str .' yang lalu'; 
+            return "$t $str yang lalu"; 
         } 
     } 
 } 
